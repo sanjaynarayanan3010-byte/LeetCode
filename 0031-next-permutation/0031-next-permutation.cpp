df2540874b7e93f1c19d4990fa07e26b0663cba7
SanjayN3010 <sanjay.narayanan3010@gmail.com>
2026-07-24 08:30:15 +0530

    Time: 0 ms (100%), Space: 15.9 MB (9.75%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int index = -1;
+        int N = nums.size();
+
+        for(int i=N-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+
+        if(index == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for(int i=N-1;i>index;i--){
+            if(nums[i] > nums[index]){
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+
+        reverse(nums.begin() + index + 1, nums.end());
     }
 };
