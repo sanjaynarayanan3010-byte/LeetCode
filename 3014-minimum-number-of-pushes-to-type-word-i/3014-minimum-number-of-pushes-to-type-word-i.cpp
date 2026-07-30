315a8ac4710b7882d8114c2228fcab18d6ca1af0
SanjayN3010 <sanjay.narayanan3010@gmail.com>
2026-07-30 11:28:12 +0530

    Time: 0 ms (100%), Space: 8.2 MB (77.73%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int ans = 0;
-        int N = word.size();
-        if(N > 24) {
-            ans += (N - 24) * 4;
-            N = 24;
+        int ans=0, n=word.size(), row=1;
+        int quotient = n/8;
+        int remainder = n %8;
+        while(quotient--) {
+            ans +=8*row;
+            row++;
+
         }
-        if(N > 16){
-            ans += (N - 16) * 3;
-            N = 16;
-        }
-        if(N > 8) {
-            ans += (N - 8) * 2;
-            N = 8;
-        }
-        ans += N;
+        ans+=remainder*row;
         return ans;
+
     }
 };
