d6bd26b026fe725e8b6e09afe49d660db75cd137
SanjayN3010 <sanjay.narayanan3010@gmail.com>
2026-08-03 10:16:36 +0530

    Time: 4 ms (32.28%), Space: 15.6 MB (53.47%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    void gen(vector<string>& ans, int N, int oCount, int cCount, string temp, bool canClose){
+    void gen(vector<string>& ans, int N, int oCount, int cCount, string temp){
         if(temp.size() == N*2){
-            if(oCount == N && cCount == 0) ans.push_back(temp);
+            ans.push_back(temp);
             return;
         }
-        if(oCount < N) gen(ans, N, oCount+1,cCount, temp+'(', 1);
-        if(cCount > 0 && canClose) {
-            if((N - cCount + 1) == oCount) gen(ans, N, oCount,cCount-1, temp+')', 0);
-            else gen(ans, N, oCount,cCount-1, temp+')', 1);
-        }
+        if(oCount < N) gen(ans, N, oCount+1,cCount, temp+'(');
+        if(cCount < oCount) gen(ans, N, oCount,cCount+1, temp+')');
 
     }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        gen(ans, n, 0, n, "", 0);
+        gen(ans, n, 0, 0, "");
         return ans;
     }
 };
