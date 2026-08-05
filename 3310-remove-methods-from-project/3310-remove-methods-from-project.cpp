83ae48a0ba55bab0a3a1b5444ca52da66d240ae1
SanjayN3010 <sanjay.narayanan3010@gmail.com>
2026-08-05 08:47:37 +0530

    Time: 1729 ms (5.17%), Space: 400.6 MB (5.17%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -6,9 +6,7 @@ public:
         for(auto curr:grid){
             mp[curr[0]].push_back(curr[1]);
         }
-        unordered_set<int> st;
-        st.insert(k);
-        vector<bool> vis(N+1,0);
+        vector<int> vis(N,0);
         queue<int> q;
         q.push(k);
         while(!q.empty()){
@@ -19,23 +17,21 @@ public:
             for(int i:mp[currNode]){
                 if(!vis[i]){
                     q.push(i);
-                    st.insert(i);
                 }
             }
         }
-
-        for(int i:st) cout<<i<<" ";
+        for(int i:vis) cout<<i<<" ";
         vector<int> ans;
         bool con = 0;
         for(auto curr:grid){
-            if(st.find(curr[0]) == st.end() && st.find(curr[1]) != st.end()){
+            if(!vis[curr[0]] && vis[curr[1]]){
                 con = 1;
                 break;
             }
         }
         if(!con){
             for(int i=0;i<N;i++) {
-                if(st.find(i) == st.end()) ans.push_back(i);
+                if(!vis[i]) ans.push_back(i);
             }
         }
         else {
