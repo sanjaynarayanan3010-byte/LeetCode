4f6eaac9b7aef1e4b658f47e280b6d591dcf893c
SanjayN3010 <sanjay.narayanan3010@gmail.com>
2026-08-26 12:30:54 +0530

    Time: 10 ms (7.3%), Space: 10.4 MB (9.32%) - LeetHub

---
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        vector<string> strs;
+        int idx = 0;
+        int N = s.size();
+        int oc = 0;
+        if(k == 1) {
+            for(char ch : s){
+                if(ch == '1') return "1"; 
+            }
+            return "";
+        }
+        for(int i=0;i<N;i++) {
+            if(s[i] == '1') {
+                idx = i;
+                break;
+            }
+        }
+        for(int i=0;i<N;i++){
+            if(s[i] == '1') oc++;
+            if(oc == k) {
+                strs.push_back(s.substr(idx, i - idx + 1));
+                idx++;
+                while(oc == k) {
+                    if(s[idx] == '1') oc--;
+                    else idx++;
+                }
+            }
+        }
+        if(strs.empty()) return "";
+        sort(strs.begin(), strs.end(), [](auto const& a, auto const& b){
+            return a.size() < b.size();
+        });
+        int minSize = strs[0].size();
+        vector<string> temp;
+        for(string t : strs) {
+            if(t.size() == minSize) temp.push_back(t);
+        }
+        sort(temp.begin(), temp.end());
+        for(string n : strs) cout<<n<<" ";
+        cout<<endl;
+        for(string n : temp) cout<<n<<" ";
+        return temp[0];
+    }
+};
