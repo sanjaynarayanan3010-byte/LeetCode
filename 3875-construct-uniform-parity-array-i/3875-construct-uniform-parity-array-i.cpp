2016985b138825be29538ec9549d8f09ebf1d91d
SanjayN3010 <sanjay.narayanan3010@gmail.com>
2026-09-02 17:44:24 +0530

    Time: 0 ms (100%), Space: 30.2 MB (67.8%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int oc = 0;
-        for(int i : nums1) {
-            if(i & 1) oc++;
-        }
-        bool ef = 1;
-        for(int i : nums1) {
-            if(i & 1) {
-                if(!oc) {
-                    ef = 0;
-                    break;
-                }
-            }
-        }
-        bool of = 1;
-        for(int i : nums1){
-            if(i % 2 == 0) {
-                if(!oc) {
-                    of = 0;
-                    break;
-                }
-            }
-        }
-        return ef || of;
+        return 1;
     }
 };
