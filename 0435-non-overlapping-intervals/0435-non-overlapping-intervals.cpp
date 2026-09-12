bf33db6bf0a614fc2fa8278e99408e67e109391a
SanjayN3010 <sanjay.narayanan3010@gmail.com>
2026-09-12 14:00:15 +0530

    Time: 45 ms (55.82%), Space: 93.9 MB (68.43%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -5,10 +5,6 @@ public:
         sort(nums.begin(), nums.end(),[](const vector<int>& a, vector<int>& b){
             return a[1] < b[1];
         });
-        for(auto const& curr : nums) {
-            for(int i : curr) cout << i << " ";
-            cout << endl;
-        }
         int size = 0;
         int le = INT_MIN;
         for(auto const& curr : nums){
