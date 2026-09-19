19f24792c4ed7e2801c50fc819ca11a3a319e654
SanjayN3010 <sanjay.narayanan3010@gmail.com>
2026-09-19 12:43:06 +0530

    Time: 46 ms (11.99%), Space: 20.2 MB (20.7%) - LeetHub

---
+++ 3913-sort-vowels-by-frequency/3913-sort-vowels-by-frequency.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+        string vow = "aeiou";
+        int N = s.size();
+        unordered_map<char,int> mp;
+        unordered_map<char, int> fi;
+        vector<pair<char, pair<int,int>>> vc;
+        for(int i=0;i<N;i++) {
+            if(vow.find(s[i]) != string::npos) {
+                if(mp[s[i]] == 0) fi[s[i]] = i;
+                mp[s[i]]++;
+            }
+        }
+        for(auto p : mp) {
+            vc.push_back({p.first,{p.second, fi[p.first]}});
+        }
+        sort(vc.begin(), vc.end(), [](auto const& a, auto const& b){
+            if(a.second.first != b.second.first) return a.second.first > b.second.first;
+            return a.second.second < b.second.second;
+        });
+
+        for(auto const& i : vc) cout << i.first << ' ' << i.second.first << " " << i.second.second << endl;
+        cout<<endl;
+        string ans = "";
+        int index = 0;
+        for(char ch : s) {
+            if(vow.find(ch) != string::npos) {
+                if(vc[index].second.first == 0) index++;
+                ans += vc[index].first;
+                vc[index].second.first--;
+            }
+            else ans += ch;
+        }
+        return ans;
+    }
+};
